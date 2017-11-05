cdf90a99c0988a5ef308091ea3c967c06cc530dd
Markus Leballeux <markus.leballeux@gmail.com>
2017-11-05 16:43:49 -0600

    Some more code cleaning.

--- wirebait.lua
+++ wirebait.lua
@@ -36,9 +36,8 @@ end
 
 -- # Wirebait Field
 local function newWirebaitField(filter, name, size, ws_field)
-    --TODO: checks
-    --checks('string', 'string', 'number', 'userdata')
-    local wb_field = {
+    --TODO: checks('string', 'string', 'number', 'userdata')
+    local wb_field = { --private data
         m_filter = filter,
         m_name = name,
         m_size = size,
@@ -61,20 +60,22 @@ local function newWirebaitField(filter, name, size, ws_field)
         return wb_field.m_wireshark_field;
     end
 
-    return {
+    local pulic_wirebait_field_interface = {
         filter = getFilter,
         name = getName,
         size = getSize,
         wsProtofield = getWiresharkProtofield
     };
+
+    return pulic_wirebait_field_interface;
 end
 
 
 
 -- # Wirebait Tree
 local function newWirebaitTree(wb_fields_map, ws_tree, buffer, position, size, parent_wb_tree)
-    local wirebait_tree = {
-        m_wb_fields_map = wb_fields_map; --reference to wirebait.created_protofields to keep track of new fields and register them
+    local wb_tree = { --private data
+        m_wb_fields_map = wb_fields_map; --reference to wirebait.created_protofields shared by all trees to keep track of new fields and register them
         m_ws_tree = ws_tree;
         m_buffer = buffer;
         m_start_position = position or 0;
@@ -85,83 +86,84 @@ local function newWirebaitTree(wb_fields_map, ws_tree, buffer, position, size, p
     }
 
     local getParent = function()
-        return wirebait_tree.m_parent;
+        return wb_tree.m_parent;
     end
 
     local getWiresharkTree = function ()
-        return wirebait_tree.m_ws_tree;
+        return wb_tree.m_ws_tree;
     end
 
     local getBuffer = function()
-        return wirebait_tree.m_buffer;
+        return wb_tree.m_buffer;
     end
 
     local getPosition = function()
-        return wirebait_tree.m_position;
+        return wb_tree.m_position;
     end
 
     local skip = function(self, byte_count)
-        if not wirebait_tree.m_is_root then
+        if not wb_tree.m_is_root then
             self:parent():skip(byte_count);
         end
-        assert(wirebait_tree.m_position + byte_count <= wirebait_tree.m_end_position , "Trying to skip more bytes than available in buffer managed by wirebait tree!")
-        wirebait_tree.m_position = wirebait_tree.m_position + byte_count;
+        assert(wb_tree.m_position + byte_count <= wb_tree.m_end_position , "Trying to skip more bytes than available in buffer managed by wirebait tree!")
+        wb_tree.m_position = wb_tree.m_position + byte_count;
     end
 
     local autoFitHighlight = function(self, is_recursive, position) --makes highlighting fit the data that was added or skipped in the tree
         position =  position or self:position();
-        assert(position >= wirebait_tree.m_start_position, "Current position is before start position!");
-        length = position - wirebait_tree.m_start_position
-        wirebait_tree.m_ws_tree:set_len(length);
-        if is_recursive and not wirebait_tree.m_is_root then
+        assert(position >= wb_tree.m_start_position, "Current position is before start position!");
+        length = position - wb_tree.m_start_position
+        wb_tree.m_ws_tree:set_len(length);
+        if is_recursive and not wb_tree.m_is_root then
             self:parent():autoFitHighlight(is_recursive, position);
         end
+    end
 
+    local findOrAddProto = function(field_key, filter, name, type_key, size, base, display_val_map);
+        if not wb_tree.m_wb_fields_map[field_key] then --adding new wb protofield if it doesn't exist
+            wb_tree.m_wb_fields_map[field_key] = wirebait.field.new(filter, name, size, Protofield[type_key](filter, name, base, display_val_map));
+        end
+        return wb_tree.m_wb_fields_map[field_key];
     end
 
     local addTree = function (self, filter, name, type_key, size, b, display_map)
-        b = b or base.DEC;
+        base = base or base.DEC;
         field_key = "f_"..name:gsub('%W','') --Removes all non alpha-num chars from name and prepend 'f_'. For instance "2 Packets" becomes "f_2Packets"
 
-        if not wirebait_tree.m_wb_fields_map[field_key] then --adding new wb protofield if it doesn't exist
-            wirebait_tree.m_wb_fields_map[field_key] = wirebait.field.new(filter, name, size, Protofield[type_key](filter, name, b, display_val_map));
-        end
-        wb_proto_field = wirebait_tree.m_wb_fields_map[field_key];
+        wb_proto_field = findOrAddProto(field_key, filter, name, type_key, size, base, display_val_map);
 
         --creating a new wireshart tree item and using it to create a new wb tree
-        new_ws_tree = wirebait_tree.m_ws_tree:add(wb_proto_field.wsProtofield(), wirebait_tree.m_buffer(wirebait_tree.m_position, wb_proto_field.size()));
-        --start_position = wirebait_tree.m_position;
-        --wirebait_tree.m_position = wirebait_tree.m_position + size;
-        return newWirebaitTree(wirebait_tree.m_wb_fields_map, new_ws_tree, wirebait_tree.m_buffer, wirebait_tree.m_position, size, self)
+        new_ws_tree = wb_tree.m_ws_tree:add(wb_proto_field.wsProtofield(), wb_tree.m_buffer(wb_tree.m_position, wb_proto_field.size()));
+        return newWirebaitTree(wb_tree.m_wb_fields_map, new_ws_tree, wb_tree.m_buffer, wb_tree.m_position, size, self)
     end
-    
+
     local addUint8 = function (self, filter, name, base, display_val_map) --display_val_map translated raw value on the wire into display value
         size = 1;
-        value = wirebait_tree.m_buffer(wirebait_tree.m_position, size):le_uint();
+        value = wb_tree.m_buffer(wb_tree.m_position, size):le_uint();
         return addTree(self, filter, name, "uint8", size, base, display_val_map), value;
     end
-    
+
     local addUint16 = function (self, filter, name, base, display_val_map) --display_val_map translated raw value on the wire into display value
         size = 2;
-        value = wirebait_tree.m_buffer(wirebait_tree.m_position, size):le_uint();
+        value = wb_tree.m_buffer(wb_tree.m_position, size):le_uint();
         return addTree(self, filter, name, "uint16", size, base, display_val_map), value;
     end
-    
+
     local addUint32 = function (self, filter, name, base, display_val_map) --display_val_map translated raw value on the wire into display value
         size = 4;
-        value = wirebait_tree.m_buffer(wirebait_tree.m_position, size):le_uint();
+        value = wb_tree.m_buffer(wb_tree.m_position, size):le_uint();
         return addTree(self, filter, name, "uint32", size, base, display_val_map), value;
     end
-    
+
     local addUint64 = function (self, filter, name, base, display_val_map) --display_val_map translated raw value on the wire into display value
         size = 8;
-        value = wirebait_tree.m_buffer(wirebait_tree.m_position, size):le_uint64();
+        value = wb_tree.m_buffer(wb_tree.m_position, size):le_uint64();
         return addTree(self, filter, name, "uint64", size, base, display_val_map), value;
     end
-    
+
     local addString = function (self, filter, name, size, base, display_val_map) --display_val_map translated raw value on the wire into display value
         size = size or 1; -- using 1 if size of string is not provided
-        value = wirebait_tree.m_buffer(wirebait_tree.m_position, size):string();
+        value = wb_tree.m_buffer(wb_tree.m_position, size):string();
         return addTree(self, filter, name, "string", size, base, display_val_map), value;
     end
 
@@ -185,14 +187,11 @@ local function newWirebaitTree(wb_fields_map, ws_tree, buffer, position, size, p
 end
 
 
-
-
-
 --[[ Using a function to create the wirebait module so that it can have 
 private state data ( 1 dissector per wirebait, and wirebait keeps track of protofields
 so as to register them automatically)
 ]]--
-local function encapsulatedWirebait() 
+local function publicWirebaitInterface() 
     local wirebait = { --wirebait state data which needs to be private
         m_created_proto_fields = {};
         m_size = 0,
@@ -207,16 +206,7 @@ local function encapsulatedWirebait()
     end
 
     function wirebait.createTreeitem(arg1, arg2, ...)
-        if arg1.__is_wirebait_struct then
-            parent_wirebait_tree = arg1;
-            assert(arg2, "Missing proto field to create new treeitem!")
-            proto_field = arg2; --//proto field for new subtree;
-            ws_tree_item = parent_wirebait_tree:wiresharkTree():add(proto_field);
-            wirebait.field.new(proto_field);
-            return newWirebaitTree(ws_tree_item or parent_wirebait_tree.wiresharkTree(), parent_wirebait_tree.__buffer(), parent_wirebait_tree.position(), parent_wirebait_tree)
-        else
-            return newWirebaitTree(wirebait.m_created_proto_fields, arg1, arg2, unpack({...}));
-        end
+        return newWirebaitTree(wirebait.m_created_proto_fields, arg1, arg2, unpack({...}));
     end
 
     function wirebait.createDissectorSingleton(name, abbrev_name)
@@ -239,9 +229,6 @@ local function encapsulatedWirebait()
     }
 end
 
-
-
-wirebait = encapsulatedWirebait() 
-
+wirebait = publicWirebaitInterface() 
 return wirebait
 
